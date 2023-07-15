93ff809ba647ab84aa87cf71b93b46416540f82e
RohitKadam220473748 <139203672+RohitKadam220473748@users.noreply.github.com>
2023-07-15 16:48:23 +1000

    Final1

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function changeText() {
-    let textsArray = ["Text 1", "Text 2", "Text 3", "Text 4", "Text 5"]
-    let number = getRandomNumberBetween(0, textsArray.length - 1)
-    console.log("Index: ", number)
-    document.getElementById("heading").innerHTML = textsArray[number];
+    let Textarray = ["Text 1", "Text 2", "Text 3", "Text 4", "Text 5"]
+    let Number = getRandomNumberBetween(0, Textarray.length - 1)
+    console.log("Index: ", Number)
+    document.getElementById("heading").innerHTML = Textarray[Number];
 }
 function getRandomNumberBetween(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
